21944d414e4fc6821a614c4ce12d3d327e543480
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-10-09 11:05:46 +0530

    added: new file in learning module

--- Cache.go
+++ Cache.go
@@ -1,18 +1,36 @@
+// Write your answer here, and then test your code.
+// Your job is to implement the getCartFromJson() method.
+
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"strings"
 )
 
-func main() {
-	var output string
-	j := 12.47
-	if j < 12.0 { 
-		output = "less" 
-	} else if j > 13 {
-		output = "more" 
-	} else {
-		output = "near" 
-	} 
-	fmt.Println(output) 
+type cartItem struct {
+	Name  string
+	Price float64
+	Quantity int
+}
+
+// getCartFromJson() returns a slice containing cartItem objects.
+func getCartFromJson(jsonString string) []cartItem {
+	// Slice declaration
+	cart := make([]cartItem, 0, 20)
+    // Your code goes here.
+	decoder := json.NewDecoder(strings.NewReader(jsonString))
+	_, err := decoder.Token()
+	if err != nil {
+		panic(err)
+	}
+    var item cartItem
+	for decoder.More() {
+		err := decoder.Decode(&item)
+		if err != nil {
+			panic(err)
+		}
+		cart = append(cart, item)
+	}
+	return cart
 }

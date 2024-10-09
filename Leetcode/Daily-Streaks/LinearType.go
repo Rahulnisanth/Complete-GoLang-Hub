b03b99e866c169ba9091276000e65267a121e461
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-10-09 09:32:08 +0530

    added: daily streak in linear type

--- Leetcode/Daily-Streaks/LinearType.go
+++ Leetcode/Daily-Streaks/LinearType.go
@@ -12,3 +12,22 @@ func minSwaps(s string) int {
     }
     return (count + 1) / 2
 }
+
+
+// MINIMUM ADD TO MAKE PARENTHESIS BALANCED :
+func minAddToMakeValid(s string) int {
+    open := 0
+    close_needed := 0
+    for _, ch := range s {
+        if ch == '(' {
+            open++
+        } else if ch == ')' {
+            if open > 0 {
+                open--
+            } else {
+                close_needed++
+            }
+        }
+    }
+    return open + close_needed
+}
